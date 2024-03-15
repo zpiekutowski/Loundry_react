3c0b85f13bfb6ccce5aefa6ad226809eb46e057f
Zbigniew Piekutowski <zbiirp@gmail.com>
2024-03-15 09:44:43 +0100

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Customer from "./components/Customer";
+import NewOrder from "./components/NewOrder";
+import NotFound from "./components/NotFound";
+import Header_main from "./components/Header_main";
+import Home from "./components/Home";
+import AddCustomer from "./components/AddCustomer";
+import EditCustomer from "./components/EditCustomer";
+import Administration from "./components/Administration";
+import ChooseCustomer from "./components/ChooseCustomer";
+import AddUnitOrder from "./components/AddUnitOrder";
+import EditUnitOrder from "./components/EditUnitOrder";
+import Orders from "./components/Orders";
+import OrderDetails from "./components/OrderDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header_main />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/customer" element={<Customer />} />
+          <Route path="/new_order" element={<NewOrder />} />
+          <Route path="/admin" element={<Administration/>} />
+          <Route path="/orders" element={<Orders/>} />
+          
+          
+          //dodatkowe
+          <Route path="/customer/add" element={<AddCustomer />} />
+          <Route path="/customer/edit/:custId" element={<EditCustomer />} />
+          <Route path="/new_order/set_customer" element={<ChooseCustomer />} />
+          <Route path="/new_order/add_unit_order" element={<AddUnitOrder />} />
+          <Route path="/new_order/edit_unit_order/:idRow" element={<EditUnitOrder />} />
+          <Route path="/orders/detils/:idOrder" element={<OrderDetails />} />
+          
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
